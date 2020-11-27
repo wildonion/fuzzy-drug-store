5c3d91c63effdbfa61ea88a5c95735dd5e6c509c
Erfan Arefi <marefimoghaddam@unixerr.com>
2020-11-27 14:36:07 +0330

    test kivy app

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ class LoginScreen(GridLayout):
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
         self.cols = 2
-        self.add_widget(Label(text='User Name'))
+        self.add_widget(Label(text='TEST'))
         self.username = TextInput(multiline=False)
         self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
+        self.add_widget(Label(text='TEST'))
         self.password = TextInput(password=True, multiline=False)
         self.add_widget(self.password)
 
