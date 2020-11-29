d944a594dc7655324757e6210922450a8180a272
Erfan Arefi <marefimoghaddam@unixerr.com>
2020-11-29 12:52:47 +0330

    scaffold db folder

---
+++ db/__init__.py
@@ -0,0 +1,18 @@
+
+
+
+import sqlite3
+
+
+class init:
+	def __init__(self):
+		self.conn = sqlite3.connect('drugstore.db')
+		self.cursor = self.conn.cursor()
+
+	def query(self, q, v):
+		res = self.cursor.execute(q, v)
+		self.conn.commit()
+		return res
+
+	def close(self):
+		self.conn.close()
